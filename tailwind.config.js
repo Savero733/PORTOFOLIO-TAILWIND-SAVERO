ccf1e7e655f24954688c28767cc0a3bec278bbc6
Savero733 <muhammad.savero77@gmail.com>
2025-03-13 20:58:31 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#92140c',  
+        secondary: '#1e1e24',
+        tertiary: '#fff8f0',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      mode: "jit",
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
